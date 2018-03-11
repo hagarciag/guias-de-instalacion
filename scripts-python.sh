9b6357991b72f2118efda6614288e0e303ca3301
jdvelasq <jdvelasq@unal.edu.co>
2018-03-11 10:12:58 -0500

    revision

--- scripts-python.sh
+++ scripts-python.sh
@@ -13,3 +13,16 @@ pip install paver
 pip install pytest
 pip install pytest-cov
 pip install mock
+
+
+## cursos
+pip install ggplot
+pip install -U git+https://github.com/yhat/ggpy.git@v0.6.6
+conda install -c conda-forge ipywidgets
+pip install plotly
+conda install seaborn
+pip install lightning-python
+conda install mpld3
+conda install networkx
+
+pip install pyspark
